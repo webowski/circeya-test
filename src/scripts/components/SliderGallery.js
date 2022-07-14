ccce0b41237129953083e3b8e03410cbc6e9155d
Aleksandr Osokin <sanvovanych@gmail.com>
2022-07-14 10:31:40 +0300

    Fix callbacks

--- src/scripts/components/SliderGallery.js
+++ src/scripts/components/SliderGallery.js
@@ -1,39 +1,44 @@
 import initSlider from './Slider'
 import initPopup from './Popup'
+import initTabs from './Tabs'
 
-let $sliderGallery = initSlider(
-	document.querySelector('.Slider.-gallery')
-)
+initTabs(() => {
+	const $sliderGallery = initSlider(
+		document.querySelector('.Slider.-gallery')
+	)
 
-const $popup = initPopup({
-	$popup: document.querySelector('#popupFullscreen'),
+	const $popup = initPopup({
+		$popup: document.querySelector('#popupFullscreen'),
 
-	showCallback(instance) {
-		let $sliderFullscreen = initSlider(
-			instance.element().querySelector('.Slider.-fullscreen')
-		)
+		showCallback(instance) {
+			let $sliderFullscreen = initSlider(
+				instance.element().querySelector('.Slider.-fullscreen')
+			)
 
-		let $captions = $sliderFullscreen.querySelectorAll('.Slide__caption')
-		$captions.forEach($caption => {
-			$caption.addEventListener('mousedown', event => {
-				$caption.classList.add('is-open')
-				$sliderFullscreen.instance.updateAutoHeight()
+			let $captions = $sliderFullscreen.querySelectorAll('.Slide__caption')
+			$captions.forEach($caption => {
+				$caption.addEventListener('mousedown', event => {
+					$caption.classList.add('is-open')
+					$sliderFullscreen.instance.updateAutoHeight()
+				})
 			})
-		})
-	}
-})
-
-
-let $gallerySlides = $sliderGallery.instance.slides
-
-$gallerySlides.forEach($slide => {
-	$slide.addEventListener('click', event => {
-		event.preventDefault()
-
-		let slideIndex = parseInt($slide.dataset.swiperSlideIndex) + 1
-		$popup.instance.show()
-
-		let $sliderFullscreen = $popup.instance.element().querySelector('.Slider.-fullscreen')
-		$sliderFullscreen.instance.slideTo(slideIndex)
+		}
 	})
+
+	let $gallerySlides = $sliderGallery.instance.slides
+
+	$gallerySlides.forEach($slide => {
+		$slide.addEventListener('click', event => {
+			event.preventDefault()
+
+			let slideIndex = parseInt($slide.dataset.swiperSlideIndex) + 1
+			$popup.instance.show()
+
+			let $sliderFullscreen = $popup.instance.element().querySelector('.Slider.-fullscreen')
+			$sliderFullscreen.instance.slideTo(slideIndex)
+		})
+	})
+
+	return true
 })
+
--- src/scripts/components/Tabs.js
+++ src/scripts/components/Tabs.js
@@ -1,41 +1,53 @@
 const $tabsNavs = document.querySelectorAll('.TabsNav')
 
-$tabsNavs.forEach($tabsNav => {
-	let $navItems = $tabsNav.querySelectorAll('.TabsNav__item')
-	let $currentItem
+function initTabs(switchCallback) {
+	let isSwitched = false
 
-	$navItems.forEach($navItem => {
-		let $link = $navItem.querySelector('a')
+	$tabsNavs.forEach($tabsNav => {
+		let $navItems = $tabsNav.querySelectorAll('.TabsNav__item')
+		let $currentItem
 
-		$link.addEventListener('click', event => {
-			event.preventDefault()
-
-			$currentItem = $navItem
-			deselectTheRest($navItems)
-			switchTab($navItem)
-		})
-	})
-
-	function switchTab($navItem) {
-		$navItem.classList.add('is-current')
-		getTabBody($navItem).classList.add('is-current')
-		return $navItem
-	}
-
-	function deselectTheRest($navItems) {
 		$navItems.forEach($navItem => {
-			if ($navItem !== $currentItem) {
-				$navItem.classList.remove('is-current')
-				getTabBody($navItem).classList.remove('is-current')
-			}
+			let $link = $navItem.querySelector('a')
+
+			$link.addEventListener('click', event => {
+				event.preventDefault()
+
+				$currentItem = $navItem
+				deselectTheRest($navItems)
+				switchTab($navItem)
+
+				if (!isSwitched) {
+					setTimeout(() => {
+						isSwitched = switchCallback()
+					}, 15)
+				}
+			})
 		})
-	}
 
-	function getTabBody($navItem) {
-		let $link = $navItem.querySelector('a')
-		let targetId = $link.getAttribute('href')
-		let $tabBody = document.querySelector(targetId)
-		return $tabBody
-	}
+		function switchTab($navItem) {
+			$navItem.classList.add('is-current')
+			getTabBody($navItem).classList.add('is-current')
+			return $navItem
+		}
 
-})
+		function deselectTheRest($navItems) {
+			$navItems.forEach($navItem => {
+				if ($navItem !== $currentItem) {
+					$navItem.classList.remove('is-current')
+					getTabBody($navItem).classList.remove('is-current')
+				}
+			})
+		}
+
+		function getTabBody($navItem) {
+			let $link = $navItem.querySelector('a')
+			let targetId = $link.getAttribute('href')
+			let $tabBody = document.querySelector(targetId)
+			return $tabBody
+		}
+
+	})
+}
+
+export default initTabs
